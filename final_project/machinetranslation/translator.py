b8767f323b98cda205d5ec40394673789900dfd6
Michel Mingroni <michelfcm@gmail.com>
2023-01-22 14:30:35 -0300

    Create translate functions

--- final_project/machinetranslation/translator.py
+++ final_project/machinetranslation/translator.py
@@ -10,16 +10,29 @@ apikey = os.environ['apikey']
 url = os.environ['url']
 version = os.environ['version']
 
-from ibm_watson import LanguageTranslatorV3
-from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
-
-authenticator = IAMAuthenticator(f'{apikey}')
+authenticator = IAMAuthenticator(apikey)
 language_translator = LanguageTranslatorV3(
-    version=f'{version}',
+    version=version,
     authenticator=authenticator
 )
 
-language_translator.set_service_url(f'{url}')
+language_translator.set_service_url(url)
 
-languages = language_translator.list_languages().get_result()
-print(json.dumps(languages, indent=2))
+# languages = language_translator.list_languages().get_result()
+# print(json.dumps(languages, indent=2))
+
+def englishToFrench(englishText):
+    translation = language_translator.translate(
+    englishText,
+    model_id='en-fr').get_result()
+    return (translation['translations'][0]['translation'])
+
+print(englishToFrench('United States of America'))
+
+def frenchToEnglish(frenchText):
+    translation = language_translator.translate(
+    frenchText,
+    model_id='fr-en').get_result()
+    return (translation['translations'][0]['translation'])
+
+print(frenchToEnglish("États-Unis d'Amérique"))
