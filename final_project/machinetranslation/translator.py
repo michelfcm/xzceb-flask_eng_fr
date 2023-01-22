e7bb6fd7845079a6b937ecadbb28ac2f919c13cf
Michel Mingroni <michelfcm@gmail.com>
2023-01-22 11:32:50 -0300

    Auth configuration

--- final_project/machinetranslation/translator.py
+++ final_project/machinetranslation/translator.py
@@ -8,14 +8,18 @@ load_dotenv()
 
 apikey = os.environ['apikey']
 url = os.environ['url']
+version = os.environ['version']
 
 from ibm_watson import LanguageTranslatorV3
 from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
 
-authenticator = IAMAuthenticator('{apikey}')
+authenticator = IAMAuthenticator(f'{apikey}')
 language_translator = LanguageTranslatorV3(
-    version='{version}',
+    version=f'{version}',
     authenticator=authenticator
 )
 
-language_translator.set_service_url('{url}')
+language_translator.set_service_url(f'{url}')
+
+languages = language_translator.list_languages().get_result()
+print(json.dumps(languages, indent=2))
